ab6bdc28ad99d6b67100d24399f6adad82609c3f
Divyesh Patil <divyeshpatil16@gmail.com>
2020-12-05 14:38:55 +0530

    UPDATED QUERY

--- Query1.sql
+++ Query1.sql
@@ -1,19 +1,18 @@
-SELECT                                    --Query perform to find Maximum and			
-					  --Minimum create_at date
-        MIN(created_at) as min_date,
-        MAX(created_at) as max_date
+WITH sub AS
+      (
+      SELECT                                    
+        MIN(created_at::timestamp) as min_date,
+        MAX(created_at::timestamp) as max_date
         FROM tutorial.yammer_users
-      
-
+      )
 SELECT
-      DATE_TRUNC('DAY', created_at) AS  first_login,  --Truncating Create_at day and time into Day as first login to make it look simpler
-      COUNT(user_id) as all_users,                    --Count total number of users present in DB
-      COUNT(CASE WHEN activated_at IS NOT NULL        --Counting total active users and displaying values 
+      DATE_TRUNC('DAY', created_at) AS  first_login,  
+      COUNT(user_id) as all_users,                   
+      COUNT(CASE WHEN activated_at IS NOT NULL        
       THEN 'Active_user' ElSE NULL END) AS total_active_users
        
       FROM tutorial.yammer_users yam
-      WHERE created_at >= '2013-01-01' AND created_at<='2014-08-31'   --specifying range of days
+      WHERE created_at >= (SELECT min_date FROM sub)
+      AND created_at<= (SELECT max_date FROM sub)
       group by 1
       order by  1 DESC
-   
-   
